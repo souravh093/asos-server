c1d0fe43f0822d2dd80c22887f2980bf25fab875
Sourav halder <souravh093@gmail.com>
2023-04-27 16:03:33 +0600

    some feature update this website

--- index.js
+++ index.js
@@ -4,15 +4,29 @@ const cors = require("cors");
 const port = 3000;
 
 app.use(cors());
-const product = require("./data/products.json");
+const products = require("./data/products.json");
+
+
 
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
+
+
 app.get("/products", (req, res) => {
-  res.send(product);
+  res.send(products);
 });
+
+
+
+app.get("/products/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedId = products.find(pro => parseFloat(pro.id) === parseFloat(id))
+  console.log(selectedId)
+  res.send(selectedId)
+})
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
